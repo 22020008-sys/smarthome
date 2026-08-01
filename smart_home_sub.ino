7add74af07b6e64c1fd5655b50fb14c6c3a82120
22020008-sys <22020008@student.bdu.edu.vn>
2026-08-01 09:20:44 +0700

    Add files via upload

--- smart_home_sub.ino
+++ smart_home_sub.ino
@@ -172,7 +172,8 @@ void xuLyHeThongTuoiNuoc() {
   
     int pct = map(rawAnalog, 4095, 1200, 0, 100);
     pct = constrain(pct, 0, 100);
-    
+    phanTramDat = pct;   // FIX: cập nhật biến global để gửi đúng giá trị sang Main
+
     Serial.printf("[GARDEN] Analog tho: %d => Do am quy doi: %d%%\n", rawAnalog, pct);
 
     static unsigned long lastSyncSoil = 0;
